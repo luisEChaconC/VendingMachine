f67cebc35df7611a3ac3263c0d6fea9c9fd2253a
Luis Esteban Chacón <luis.chaconchaves@ucr.ac.cr>
2025-07-12 03:25:22 -0600

    feat: add product dictionary initialization in Program.cs
    - Dictionary for product persistence simulation
    - Singleton instance. One instance across the application

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,7 +1,26 @@
+using Backend.Domain.Product;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IDictionary<Guid, ProductModel>>(provider =>
+{
+    var productDictionary = new Dictionary<Guid, ProductModel>();
+
+    var product1 = new ProductModel("Coca Cola", 800, 10, "coca-cola.png");
+    var product2 = new ProductModel("Pepsi", 750, 8, "pepsi.png");
+    var product3 = new ProductModel("Fanta", 950, 10, "fanta.png");
+    var product4 = new ProductModel("Sprite", 975, 15, "sprite.png");
+
+    productDictionary.Add(product1.Id, product1);
+    productDictionary.Add(product2.Id, product2);
+    productDictionary.Add(product3.Id, product3);
+    productDictionary.Add(product4.Id, product4);
+
+    return productDictionary;
+});
 
 var app = builder.Build();
 
