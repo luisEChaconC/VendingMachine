ac67ec8cb5cc3ca2ae9686c325a1e5b9813397a4
Esteban Chacón <luis.chaconchaves@ucr.ac.cr>
2025-07-12 07:10:22 +0000

    chore: add initial backend structure (#1)

---
+++ backend/src/Domain/Product/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Domain.Product
+{
+    public class ProductModel(string name, decimal price, int stock, string imagePath, Guid? id = null)
+    {
+        public Guid Id { get; } = id ?? Guid.NewGuid();
+        public string Name { get; } = name;
+        public decimal Price { get; } = price;
+        public int Stock { get; } = stock;
+        public string ImagePath { get; } = imagePath;
+    }
+}
