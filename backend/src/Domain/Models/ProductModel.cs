ac6c7f559441e5b718a36494466e4464698d83c1
Luis Esteban Chacón <luis.chaconchaves@ucr.ac.cr>
2025-07-12 22:06:47 -0600

    refactor: add setters for ProductModel properties to be able to use Fixture library for unit tests

--- backend/src/Domain/Models/ProductModel.cs
+++ backend/src/Domain/Models/ProductModel.cs
@@ -2,9 +2,9 @@
 
 public class ProductModel(string name, decimal price, int stock, string imagePath, Guid? id = null)
 {
-    public Guid Id { get; } = id ?? Guid.NewGuid();
-    public string Name { get; } = name;
-    public decimal Price { get; } = price;
-    public int Stock { get; } = stock;
-    public string ImagePath { get; } = imagePath;
+    public Guid Id { get; set; } = id ?? Guid.NewGuid();
+    public string Name { get; set; } = name;
+    public decimal Price { get; set; } = price;
+    public int Stock { get; set; } = stock;
+    public string ImagePath { get; set; } = imagePath;
 }
