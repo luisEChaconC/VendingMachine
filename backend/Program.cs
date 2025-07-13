f4395c1e8ed410fd65507cdc0b517d52704ad258
Luis Esteban Chacón <luis.chaconchaves@ucr.ac.cr>
2025-07-12 23:14:11 -0600

    feat(backend): add CORS to allow requests from Vue app

--- backend/Program.cs
+++ backend/Program.cs
@@ -12,6 +12,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowVueApp",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:8080")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 // Presentation layer dependencies
 // Queries
 builder.Services.AddScoped<IGetAllProductsQuery, GetAllProductsQuery>();
@@ -71,6 +82,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowVueApp");
 
 app.MapControllers();
 
